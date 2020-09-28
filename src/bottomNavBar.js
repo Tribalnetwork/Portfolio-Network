b42336476f81991dc0f4921c45fc5062641f4549
jreid2454 <jreid2454@yahoo.com>
2020-09-28 16:30:20 -0400

    commit to un conflict

--- src/bottomNavBar.js
+++ src/bottomNavBar.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
@@ -33,4 +34,41 @@ export default class BottomNavBar extends React.Component{
             </div>
         )
     }
+=======
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+
+export default class BottomNavBar extends React.Component{
+    constructor(){
+        super()
+    }
+
+    ulStyle = {
+        position: "fixed",
+        bottom: "1vh",
+        width: "80%",
+        margin: "auto",
+        listStyleType: "none"
+   }
+
+   liStyle = {
+       float: "left",
+       width: "25%",
+       margin: "auto"
+   }
+
+    render(){
+
+        return(
+            <div>
+                <ul style={this.ulStyle}>
+                    <li style={this.liStyle}><Link to={"/search"}>Search</Link></li>
+                    <li style={this.liStyle}><button>{"clickMe"}</button></li>
+                    <li style={this.liStyle}><button>{"clickMe"}</button></li>
+                    <li style={this.liStyle}><button>{"clickMe"}</button></li>
+                </ul>
+            </div>
+        )
+    }
+>>>>>>> f5b5085... search function, and skeleton of bottom nav bar
 }
