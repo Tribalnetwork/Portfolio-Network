354ab8bfc9b691e4ef1d712a599e4e1ae769d6dd
jreid2454 <jreid2454@yahoo.com>
2020-10-09 10:02:50 -0400

    updated styling on bottomNavBar and Search, updated connect feature

--- src/components/bottomNavBar.js
+++ src/components/bottomNavBar.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import Home from "../bottomNavBarImg/home.jpg"
-import Tribal from "../bottomNavBarImg/tribal.jpg"
-import Settings from "../bottomNavBarImg/settings.jpg"
-import Social from "../bottomNavBarImg/social.jpg"
-import Search from "../bottomNavBarImg/search.jpg"
+import Home from "../bottomNavBarImg/Home.jpg"
+import Tribal from "../bottomNavBarImg/Tribal.jpg"
+import Settings from "../bottomNavBarImg/Settings.jpg"
+import Social from "../bottomNavBarImg/Social.jpg"
+import Search from "../bottomNavBarImg/Search.jpg"
 
 export default class BottomNavBar extends React.Component{
     constructor(){
         super()
+    }
+
+    divStyle = {
+        backgroundColor: "#121212",
+        marginLeft: "1vw",
+        marginRight: "1vw",
+        padding: "0",
+        position: "fixed",
+        bottom: "1vh",
+        width: "98%",
+        height: "7vh",
+        borderRadius: "10px"
     }
 
     ulStyle = {
@@ -27,27 +39,33 @@ export default class BottomNavBar extends React.Component{
        justifySelf: "center",
        gridRow: "1",
        width: "6vw",
-       fontSize: "2vw"
+       fontSize: "2vw",
+       maxMarginBottom: "1vh",
+       minMarginBottom: "0"
    }
 
    imgStyle = {
         minWidth: "3vw",
-        maxWidth: "6vw",
+        maxWidth: "9vw",
         minHeight: "3vw",
-        maxHeight: "6vh",
+        maxHeight: "9vh",
    }
 
 
     render(){
 
         return(
-            <ul style={this.ulStyle}>
+        
+            <div style={this.divStyle} className={"navbardiv"}>
+                <ul style={this.ulStyle}>
                     <li style={this.liStyle}><Link to={"/home"}><img style={this.imgStyle} src={Home}/></Link></li>
                     <li style={this.liStyle}><Link to={"/search"}><img style={this.imgStyle} src={Search}/></Link></li>
                     <li style={this.liStyle}><Link to={"/streams"}><img style={this.imgStyle} src={Tribal}/></Link></li>
                     <li style={this.liStyle}><Link to={"/profile"}><img style={this.imgStyle} src={Social}/></Link></li>
                     <li style={this.liStyle}><Link to={"/settings"}><img style={this.imgStyle} src={Settings}/></Link></li>
-            </ul>
+                </ul>
+            </div>
+        
         )
     }
 }
