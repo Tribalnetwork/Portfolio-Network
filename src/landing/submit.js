ba1a19bb36e005d90f4a54e2ce8400cace02daa1
Nechir dev <nechirvan.abdullah@outlook.com>
2021-01-09 22:28:03 +0300

    initail commit

--- src/landing/submit.js
+++ src/landing/submit.js
@@ -39,7 +39,8 @@ var requestedId = JSON.stringify(urlParams.get("id"));
 var requestingName = localStorage.getItem('CognitoIdentityServiceProvider.1t8oqsg1kvuja9u9rvd2r1a6o4.LastAuthUser');
 var requestingUserData = localStorage.getItem(`CognitoIdentityServiceProvider.1t8oqsg1kvuja9u9rvd2r1a6o4.${requestingName}.userData`);
 var parsed = JSON.parse(requestingUserData);
-var requestingId = JSON.stringify(parsed.UserAttributes[0].Value)
+// var requestingId = JSON.stringify(parsed.UserAttributes[0].Value)
+var requestingId = 10;
 
 // Film Genre
 const options = [
