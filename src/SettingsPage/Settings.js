271c01182b5191abba92220af5619a336852d11b
Neck <nechirvan.abdullah@outlook.com>
2021-02-01 11:09:25 -0500

    formating and styling notification component - first satge

--- src/SettingsPage/Settings.js
+++ src/SettingsPage/Settings.js
@@ -6,6 +6,9 @@ import { Button, IconButton } from "@material-ui/core";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
+import ClearIcon from '@material-ui/icons/Clear';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
 function Settings() {
   let history = useHistory();
   return (
@@ -1372,102 +1375,148 @@ export { Account };
 // }
 
 function Notifications() {
-  function generatenotify(_from, _subject, _body, _time,_read=false){
-    return {from:_from, subject:_subject, body:_body, time:_time, read:_read}
+  // generating objects
+  // for future will need to bring data from API
+  function generatenotify(_from, _subject, _body, _time, _read = false) {
+    return { from: _from, subject: _subject, body: _body, time: _time, read: _read }
   }
-  let testdata=[
-    generatenotify("From1", "Subject1", "Body1",  new Date(2020, 11, 9, 2, 3, 4))
-    ,generatenotify("From6", "Subject6 LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OIJDASOI DJASDIAJ OISADJOAIS DJ", "Body6",  new Date(2020, 16, 9, 2, 3, 4))
-    ,generatenotify("From2", "Subject2", "Body2",  new Date(2020, 12, 9, 2, 3, 4), true)
-    ,generatenotify("From4", "Subject4", "Body4",  new Date(2020, 14, 9, 2, 3, 4))
-    ,generatenotify("From5", "Subject5", "Body5",  new Date(2020, 15, 9, 2, 3, 4), true)
-
-
+  // template Data
+  let testdata = [
+    generatenotify("Tim Halbrook", "Hey how are you doing!", "Just checking in to see if you still want to have a meeting next Thursday. We were able to get the new filming location and it'll be ready for us next month! Get back to me when you can. Thanks, Tim", new Date(2020, 11, 9, 2, 3, 4))
+    , generatenotify("Naura Cook", "Resechedule business team", "Body6", new Date(2020, 16, 9, 2, 3, 4))
+    , generatenotify("Tina Fairbrooks", "Gig inquiry", "Body2", new Date(2020, 12, 9, 2, 3, 4), true)
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "Connection Request", "Body4", new Date(2020, 14, 9, 2, 3, 4))
+    , generatenotify("Tribal Network", "You just got a star", "Body5", new Date(2020, 15, 9, 2, 3, 4), true)
   ]
+  // states
   let history = useHistory();
-  const [id, setid] = useState(-1); 
-  const [read, setread] = useState(false); 
-  const [data, setdata] = useState(testdata.sort((a,b)=>a.time > b.time ? -1 : 1)); 
+  const [id, setid] = useState(-1);
+  const [read, setread] = useState(false);
+  const [data, setdata] = useState(testdata.sort((a, b) => a.time > b.time ? -1 : 1));
 
-
-  var header=(
-
-    <div className="pageTitle">
-      <IconButton edge="end" color="white" onClick={() => history.goBack()}><ArrowBackIosIcon className="backIcon" /></IconButton>
+  // notification page header
+  var header = (
+    <div className="pageTitle notification-header">
+      <IconButton
+        edge="end"
+        color="white"
+        onClick={() => history.goBack()}>
+        <ArrowBackIosIcon className="backIcon" />
+      </IconButton>
       <h2 className="text">Notifications</h2>
     </div>
   )
-  console.log(read)
-  if(!read || id == -1){
-    return(<body>
-      {header}
-      <table className="not-table">
-        {data.map((notify, index) =>{
-          if(notify.read==true)
-            return(
-              <tr className="not" style={{backgroundColor: 'grey'}}> 
-                <td className="not-entry" > <input type="radio" id={index} onClick={() => setid(index)} checked={index==id}/>  </td>
-                <td className="not-entry"  >  {notify.from} </td> 
 
-                <td className="not-entry">  {notify.subject}  </td>
-              </tr>
-            )
-          else
-            return(
-              <tr className="not" > 
-                <td className="not-entry" > <input type="radio" id={index} onClick={() => setid(index)} checked={index==id}/>  </td>
-                <td className="not-entry">  {notify.from} </td> 
+  // console.log("read:"+ read)
+  // console.log("data:"+ data)
 
-                <td className="not-entry">  {notify.subject}  </td>
-              </tr>
-            )
-        }
-        )}
-      </table>
-      <div >
-        <button onClick={() =>{
-          if(id!=-1){
-            let tmp = data
-            tmp.splice(id,1)
-            setdata(tmp)
-            setid(-1)
-            setread(false)
+  // if message is 
+  if (!read || id === -1) {
+    return (
+        <div className='notifications-page'>
+          {header}
+          {/* notifications */}
+          <main className='notification-box'>
+            <div className="notifications-container">
+              {
+                data.map((notify, index) => {
+                  // if notify.read == true
+                  // notify is the current notification
+                  // index is the array index starting from 0
+                  // console.log(notify, index)
 
-          }
-        }}> Trash </button>
-        <button onClick={() => {
-          if(id!=-1)
-            setread(true)
-
-        }}> Read currently selected notification </button>
-      </div>
-    </body>)
+                  // notifications that have not been read yet
+                  if (notify.read)
+                    return (
+                      <div className="notification-row" style={{ backgroundColor: '#222222' }}>
+                        <div className="notification-column" >
+                          {/* <input type="radio" id={index} onClick={() => setid(index)} checked={index === id} /> */}
+                          <div className='container'>
+                            <div className='check'
+                              id={index}
+                              onClick={() => setid(index)}
+                              style={index === id ? { background: "repeating-linear-gradient(to left,#D3C095,#A07923)", border: '4px solid #222222' } : { backgroundColor: "#222222" }}>
+                            </div>
+                          </div>
+                        </div>
+                        <p className="notification-column"  >  {notify.from} <span className='notification-bar'>|</span> {notify.subject} </p>
+                      </div>
+                    )
+                  // notification that have been read
+                  else
+                    return (
+                      <div className="notification-row" >
+                        <div className="notification-column" >
+                          {/* <input type="radio" id={index} onClick={() => setid(index)} checked={index === id} /> */}
+                          <div className='container'>
+                            <div className='check'
+                              id={index}
+                              onClick={() => setid(index)}
+                              style={index === id ? { background: "repeating-linear-gradient(to left,#D3C095,#A07923)", border: '4px solid #15161B' } : { backgroundColor: "#15161B" }}>
+                            </div>
+                          </div>
+                        </div>
+                        <p className="notification-column">  {notify.from} <span className='notification-bar'>|</span> {notify.subject}</p>
+                      </div>
+                    )
+                }
+                )}
+            </div>
+            {/* buttons */}
+            <div className='btns-container'>
+              <button
+                className='clear-btn'
+                onClick={() => {
+                  if (id !== -1) {
+                    let tmp = data
+                    tmp.splice(id, 1)
+                    setdata(tmp)
+                    setid(-1)
+                    setread(false)
+                  }
+                }}> <ClearIcon style={{color:"white"}} fontSize='large'/> </button>
+              <button
+                className='read-btn'
+                onClick={() => {
+                  if (id !== -1)
+                    setread(true)
+                }}> <MailOutlineIcon style={{color:"white"}} fontSize='large'/> </button>
+            </div>
+          </main>
+        </div>
+    )
   }
-  else{
+  else {
     let entry = data[id]
-    return(<body>
+    return (<body>
       {header}
       <h1> From: {entry.from} </h1>
       <p> {entry.body} </p>
 
       <div >
-        <button onClick={() =>{
-          if(id!=-1){
+        <button onClick={() => {
+          if (id !== -1) {
             let tmp = data
-            tmp.splice(id,1)
+            tmp.splice(id, 1)
             setdata(tmp)
             setid(-1)
             setread(false)
-
           }
         }}> Trash current notification</button>
         <button onClick={() => {
           setid(-1)
           setread(false)
           let tmp = data;
-          tmp[id].read=true
+          tmp[id].read = true
           setdata(tmp)
-        } }> Check rest of notifications </button>
+        }}> Check rest of notifications </button>
       </div>
     </body>)
 
