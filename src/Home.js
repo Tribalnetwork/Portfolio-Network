81a40667e1d5d625de69f2cc35212e0dc0bb28a8
junjie-w <junjiewu.brooke@gmail.com>
2021-02-02 21:03:14 +0100

    modified homepage on tribalBeta

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Amplify from 'aws-amplify';
 import { API, graphqlOperation } from 'aws-amplify'
 import { listFilms, listLiveStreams } from './graphql/queries'
@@ -8,24 +8,26 @@ import { Link } from "react-router-dom";
 import UserContext from './UserContext'
 import { Helmet } from 'react-helmet'
 import ReactPlayer from 'react-player'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from './Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-//import './Home.css';
 import HorizontalScrollerCircular from "./components/HorizontalScrollerCircular";
 import TrendingNow from "./TrendingNow";
 import ContinueWatching from "./ContinueWatching";
 import MyList from "./MyList";
-import { ReactComponent as ExploreLogo } from './icons/Explore.svg';
-import { ReactComponent as MyStudioLogo } from './icons/myStudio.svg';
-import { ReactComponent as StarRating } from './icons/Rate.svg'
+//import { ReactComponent as ExploreLogo } from './icons/Explore.svg';
+//import { ReactComponent as MyStudioLogo } from './icons/myStudio.svg';
+//import { ReactComponent as StarRating } from './icons/Rate.svg'
+import { ReactComponent as RatingButton } from './icons/RateButton_tribalBeta.svg';
+import { ReactComponent as SubmitButton } from './icons/SubmitButton_tribalBeta.svg';
+import { ReactComponent as StarRatingIcon } from './icons/starRating__tribalBeta.svg'
 import Gigs from './Gigs/Gigs';
 import { Button as MaterialUiButton } from '@material-ui/core';
+import './Home.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 Amplify.configure(awsconfig);
+
 export default class Home extends React.Component {
 
   static contextType = UserContext
@@ -69,8 +71,9 @@ export default class Home extends React.Component {
     const isAuthenticated = this.context.user && this.context.user.username ? true : false
     const isLoaded = this.context.isLoaded
     const hasAccess = this.context.hasAccess || this.context.remainingVODTime > 0
+
     return (
-      <div>
+      <div className="home__tribalBeta">
         <Helmet>
           <meta charSet="utf-8" />
           <title>Home - Tribal Network</title>
@@ -92,31 +95,136 @@ export default class Home extends React.Component {
                     width="100%"
                     height="100%"
                   />
-                  <div className="video-name-wrapper">
-                    <p className="video-name">{this.state.videoName}</p>
-                  </div>
+                  {/*<div className="video-name-wrapper">
+                    <p className="video-name videoName__tribalBetaHome">{this.state.videoName}</p>
+                  </div>*/}
+                </div>
+                <div className="video-name-wrapper">
+                  <p className="video-name title__tribalBetaHome">{this.state.videoName}</p>
                 </div>
                 <div className="functionbar-wrapper">
                   <Grid container justify="space-between" alignItems="center">
                     <Grid item>
                       {/*<Link to={'/explore'}><ExploreLogo></ExploreLogo></Link>*/}
-                      <StarRating></StarRating>
+                      {/*<StarRating></StarRating>*/}
+                      {/*<Link to={'/sub'} style={styles.buttonLink}>*/}
+                      {/*<RatingButton className="RateAndSubmit__tribalBeta" />*/}
+                      <Popup
+                        trigger={open => (
+                          <RatingButton className="RateAndSubmit__tribalBeta" />
+                        )}
+                        //position="center center"
+                        closeOnDocumentClick
+                      >
+                        <div className="ratingPopup__tribalBetaHome">
+                          <h3 className="titlePopup__tribalBetaHome">Rate This Film</h3>
+                          <div className="overall__tribalBetaHome">
+                            <span className="overallStar__tribalBetaHome">Overall</span>
+                            <span>
+                              <StarRatingIcon />
+                              <StarRatingIcon />
+                              <StarRatingIcon />
+                              <StarRatingIcon />
+                              <StarRatingIcon />
+                            </span>
+                          </div>
+                          <div className="popupBottom">
+                            <span className="cancel__tribalBetaHome">Cancel</span>
+                            <span className="send__tribalBetaHome">Send</span>
+                          </div>
+                        </div>
+                      </Popup>;
+                      {/*</Link>*/}
                     </Grid>
                     <Grid item>
                       {/*<Link to={'/mystudio/myFolder'}><MyStudioLogo></MyStudioLogo></Link>*/}
-                      <Link to={'/submit'} style={styles.buttonLink}>
-                        <MaterialUiButton style={styles.MaterialUiButton}>Submit</MaterialUiButton>
-                      </Link>
+                      {/*<Link to={'/submit'} style={styles.buttonLink}>*/}
+                      {/*<MaterialUiButton style={styles.MaterialUiButton}>Submit</MaterialUiButton>*/}
+                      {/*<SubmitButton className="RateAndSubmit__tribalBeta" />*/}
+                      {/*<SubmitButton className="RateAndSubmit__tribalBeta" />
+                    */}
+                      <Popup
+                        trigger={open => (
+                          <SubmitButton className="RateAndSubmit__tribalBeta" />
+                        )}
+                        //position="center center"
+                        closeOnDocumentClick
+                      >
+                        <div className="ratingPopup__tribalBetaHome">
+                          <div className="overall__tribalBetaHome">
+                            <span className="submit__tribalBetaHome">Log in on Desktop to Submit</span>
+                          </div>
+                          <div className="ok__popupBottom">
+                            <Link to={'/submit'} style={styles.buttonLink} className="link__okText">
+                              <p className="okText__popupBottom">OK</p>
+                            </Link>
+                          </div>
+                        </div>
+                      </Popup>;
+                      {/*</Link>*/}
                     </Grid>
                   </Grid>
                 </div>
                 <div className="trendy-wrapper">
-                  <p>Trending Live</p>
+                  {/*<p className="title__tribalBetaHome">Trending Live</p>*/}
+                  <p className="title__tribalBetaHome">Comedy</p>
                   <HorizontalScrollerCircular list={this.state.livestreams} />
                 </div>
-                <ContinueWatching></ContinueWatching>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Action</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Thriller</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Romance</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Sci-fi</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Drama</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Animation</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Music</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Horror</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Experimental</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Trailers</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Documentary</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Sports</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                <div className="trendy-wrapper">
+                  <p className="title__tribalBetaHome">Talks</p>
+                  <HorizontalScrollerCircular list={this.state.livestreams} />
+                </div>
+                {/*<ContinueWatching></ContinueWatching>
                 <TrendingNow></TrendingNow>
-                <MyList></MyList>
+                <MyList></MyList>*/}
               </div>
 
             ) : (
@@ -143,7 +251,7 @@ export default class Home extends React.Component {
               )
           }
         </div>
-      </div>
+      </div >
 
 
     )
@@ -168,6 +276,6 @@ const styles = {
   streamText: { fontSize: 14, marginBottom: 0 },
   filmDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' },
-  MaterialUiButton: { backgroundImage: 'linear-gradient(to right, #CAAA65,#A07923)', color: 'white', padding: '9px 59px', fontSize: '1.05rem', textTransform: 'inherit', borderRadius: '12px', marginRight: "10px" },
-  buttonLink: { textDecoration: 'none' }
+  //MaterialUiButton: {backgroundImage: 'linear-gradient(to right, #CAAA65,#A07923)', color: 'white', padding: '9px 59px', fontSize: '1.05rem', textTransform: 'inherit', borderRadius: '12px', marginRight: "10px" },
+  //buttonLink: {textDecoration: 'none' }
 }
