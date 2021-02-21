118255df4e1bb65ea5963c7873f5fc357958918e
jreid2454 <jreid2454@yahoo.com>
2021-02-21 01:04:32 -0500

    made getfilmList functions

--- src/components/GetFilmList.js
+++ src/components/GetFilmList.js
@@ -2,7 +2,39 @@ import React from "react"
 import axios from "axios"
 import FilmFrame from "./filmFrame2"
 
-export default class GetFilmList extends React.Component{
+export default class GetFilms {
+        
+    // gets a list of film info from RDS for all films
+    getAllFilms(){
+        axios({
+            url: "https://ulqk0b3anh.execute-api.us-east-1.amazonaws.com/default/Get-Film-List",
+            method: 'get',
+          })
+          .then((res) => {
+              return res.data.body
+          })
+        .catch(err => console.log("error message: " + err))
+    }
+
+        //pass the film_id as data to get a url for the film
+    getUrl = () => {
+        let FilmKey = {
+          id: this.id // put the film id here 
+        }
+        let theData = JSON.stringify(FilmKey);
+        axios({
+          url: "https://2ajlr7txqa.execute-api.us-east-1.amazonaws.com/default/Get_Film_From_S3",
+          method: "post",
+          data: theData
+        })
+        .then((res) => {
+          console.log("the response: " + JSON.stringify(res.data.body.url));
+        })
+        .catch(err => console.log("the erreor: " + err))
+      }
+}
+
+/*export default class GetFilmList extends React.Component{
     constructor(){
         super()
         this.state = {
@@ -45,4 +77,4 @@ export default class GetFilmList extends React.Component{
             </div>
         )
     }
-}
+}*/
