ea1c714141374c8ed399f9a20d03d0159c430fba
Zhongwei Li <43942535+ZhongweiL@users.noreply.github.com>
2022-02-12 11:33:24 -0500

    Explore page (#280)
    * add explore component and route
    
    * add explore page that shows a list of genres, and add the page in the router
    
    Co-authored-by: Zhongwei Li <li@Zhongweis-Air.lan>

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -23,6 +23,7 @@ import ContactUsTribalBeta from "../pages/settings/ContactUs";
 
 import Signin from '../pages/authentication/SignIn'
 
+import Explore from '../pages/explore/Explore'
 
 import { useTheme } from "@material-ui/core/styles";
 import { useMediaQuery } from "@material-ui/core";
@@ -153,6 +154,7 @@ const Routes = () => {
           <Route path="/myStudio" exact component={FeatureComingSoon} />
           <Route path="/myStudio/myFolder" exact component={FeatureComingSoon} />
           <Route path="/myStudio/toDoList" exact component={FeatureComingSoon} />
+          <Route path="/explore" exact component={Explore} />
           <PrivateRoute path="/upload" exact component={FeatureComingSoon} />
           <PrivateRoute path="/streams" component={FeatureComingSoon} />
           <PrivateRoute path="/watch" component={Watch} />
