446cd777cb450bb55a70fa36243669bd4fb17a1d
Neck <66407817+Nechir-89@users.noreply.github.com>
2022-02-27 09:37:04 -0500

    Update Explore.js

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,29 +1,10 @@
-import React, { useState, useEffect }from "react";
-import axios from 'axios';
-
+import React from "react";
+import Genres from './Genres'
 function Explore() {
-    const [genres, setGenres] = useState([])
-
-    useEffect(() => {
-        // getGenreList from backend
-        function getGenreList() {
-            // 1: get genre list from back end
-            axios.get('https://6evel85j84.execute-api.us-east-1.amazonaws.com/default/getGenresList')
-            .then(resp => {
-                let options = resp.data.body.map(item => { return { value: item.genre_id, label: item.genre_desc } })
-                // 2: set genres to select input
-                setGenres(options)
-            }).catch(err => {
-                console.log(err)
-            })
-        }
-        getGenreList();
-    })
-
 
     return (
         <div>
-            {genres.map(genre => <div>{genre.label}</div>)}
+            <Genres />
         </div>
     )
 }
