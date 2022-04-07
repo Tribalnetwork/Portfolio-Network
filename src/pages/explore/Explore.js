a5fa200083e1e46b16723ab40b32805e9e610296
Zhongwei Li <li@Zhongweis-Air.lan>
2022-04-06 22:43:07 -0400

    use flexbox to handle header styling

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -6,6 +6,13 @@ function Explore() {
     return (
         <div>
             <div className="explorePageContainer">
+                <header className="explorePageHeader">
+                    <div className="title">
+                        <h1>Explore</h1>
+                        <p>Discover all the fun things</p>
+                    </div>
+                    <button> Watch Random</button>
+                </header>
                 <Genres />
             </div>
         </div>
