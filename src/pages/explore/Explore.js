ac24912954e3342369f9e1854704cd7f0cff3af3
Zhongwei Li <li@Zhongweis-Air.lan>
2022-04-14 22:04:05 -0400

    organize indentation

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -9,23 +9,21 @@ import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 function Explore() {
     let history = useHistory();
     return (
-        
-            <div className="explorePageContainer">
-                <header className="explorePageHeader">
-                    <IconButton edge="end" color="white" onClick={() => history.goBack()}>
-                        <ArrowBackIosIcon className="backIcon" style={{ color: "white" }} />
-                    </IconButton>
-                    <div className="title">
-                        <h1>Explore</h1>
-                        <p>Discover all the fun things</p>
-                    </div>
-                    <div className="watchRandomBtn">
-                        <Link className="watchRandomLink" to="/watch">Watch Random</Link>
-                    </div>
-                </header>
-                <Genres />
-            </div>
-        
+        <div className="explorePageContainer">
+            <header className="explorePageHeader">
+                <IconButton edge="end" color="white" onClick={() => history.goBack()}>
+                    <ArrowBackIosIcon className="backIcon" style={{ color: "white" }} />
+                </IconButton>
+                <div className="title">
+                    <h1>Explore</h1>
+                    <p>Discover all the fun things</p>
+                </div>
+                <div className="watchRandomBtn">
+                    <Link className="watchRandomLink" to="/watch">Watch Random</Link>
+                </div>
+            </header>
+            <Genres />
+        </div>
     )
 }
 
