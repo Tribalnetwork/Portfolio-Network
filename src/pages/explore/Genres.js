e1f3edcfb7972a1039522675ab6e00857f1afa18
Zhongwei Li <li@Zhongweis-Air.lan>
2022-04-22 16:55:25 -0400

    show different icons based on the genre

--- src/pages/explore/Genres.js
+++ src/pages/explore/Genres.js
@@ -1,27 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './explore.css';
-
 import {getGenreList} from '../../middleware/APIs';
-import Action from '../../assets/images/genresIcons/actionsi.svg';
 import { Link } from "react-router-dom";
-/*
-import Horror from '../../assets/images/genresIcons/horror.svg';
-import Suspense from '../../assets/images/genresIcons/actionsi.svg';
-import Drama from '../../assets/images/genresIcons/drama.svg';
-import Comedy from '../../assets/images/genresIcons/comedy.svg';
-import Thriller from '../../assets/images/genresIcons/Thriller.svg';
-import Fantasy from '../../assets/images/genresIcons/actionsi.svg';
-import Mystery from '../../assets/images/genresIcons/actionsi.svg';
-import SciFi from '../../assets/images/genresIcons/si-fi.svg';
-import Adventure from '../../assets/images/genresIcons/actionsi.svg';
-import Crime from '../../assets/images/genresIcons/actionsi.svg';
-import Action from '../../assets/images/genresIcons/actionsi.svg';
-import Documentary from '../../assets/images/genresIcons/documentory.svg';
-import Romance from '../../assets/images/genresIcons/romance.svg';
-import Animated from '../../assets/images/genresIcons/animation.svg';
-import Trailer from '../../assets/images/genresIcons/actionsi.svg';
-import Experimental from '../../assets/images/genresIcons/actionsi.svg';
-*/
+
 export default function Genres() {
   const [genres, setGenres] = useState([]);
 
@@ -41,7 +22,7 @@ export default function Genres() {
       {genres.map(genre => {
         return (
           <Link className="exploreGenre" to={`/explore/${genre.label}`}>
-            <img src={Action} alt="genre"></img>
+            <img src={`/images/exploreIcons/${genre.label}.svg`} alt="genre"></img>
             <p>{genre.label}</p>
           </Link>
         )
