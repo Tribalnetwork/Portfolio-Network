55b5c098dd4bb3f01717037f4264cd7c39f992bc
Taylie Whitworth <105079720+tayliewhitworth@users.noreply.github.com>
2022-12-09 14:10:17 -0700

    Added files I have created
    This is the new React code that is from Daniels designs.

---
+++ tribaliii/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Login from './components/Login'
+import Home from './container/Home';
+import About from './components/About'
+import Projects from './components/Projects'
+import CreateAccount from './components/CreateAccount';
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/create' element={<CreateAccount />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
